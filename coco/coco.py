67e9bb805281d385606e137a9c6a8c330a47c0f7
Alejandro Navarini <alejandronavarini@gmail.com>
2023-05-14 15:44:04 -0300

    Fix coco file reference

--- coco/coco.py
+++ coco/coco.py
@@ -1,4 +1,4 @@
 def get_classnames() -> list[str]:
-    with open("coco.names", "r") as f:
+    with open("coco/coco.names", "r") as f:
         classes = [line.strip() for line in f.readlines()]
     return classes
